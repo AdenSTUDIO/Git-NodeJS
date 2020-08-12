0bc91b437878b4c95f77629b5cabe9523c7b216e
AdenSTUDIO <adenstudio.mail@gmail.com>
2020-08-12 09:21:02 +0700

    GITHUB Commit

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8888; // GITHUB Commit
+app.use(express.static('public'));
+app.use('/v', express.static('views'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(__dirname + '/views/index.html');
 })
 
 app.listen(port);
